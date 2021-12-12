8e0654b7496066390b04edf8a19a32a2db6b780e
kx <kaixuan.sun@boonray.com>
2021-12-12 18:13:41 +0800

    快速排序

---
+++ algorithm/sorts/quickSort.go
@@ -0,0 +1,44 @@
+package sorts
+
+// 递归快排
+func qSort(arr *[]int, low, high int) {
+
+	i := low
+	j := high
+
+	if i < j {
+		temp := (*arr)[low]
+		for i < j {
+
+			for i < j && (*arr)[j] > temp {
+				j--
+			}
+			if i < j {
+				(*arr)[i] = (*arr)[j]
+				i++
+			}
+
+			for i < j && (*arr)[i] < temp {
+				i++
+			}
+			if i < j {
+				(*arr)[j] = (*arr)[i]
+				j--
+			}
+
+		}
+
+		(*arr)[j] = temp
+		qSort(arr, low, i-1)
+		qSort(arr, i+1, high)
+	}
+
+}
+
+// 快速排序
+func QuickSort(arr []int) []int {
+	i := 0
+	j := len(arr) - 1
+	qSort(&arr, i, j)
+	return arr
+}
