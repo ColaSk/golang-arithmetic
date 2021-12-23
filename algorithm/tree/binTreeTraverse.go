70ba1cd6901d79c9672c2482a63f81c6663dcb80
kx <kaixuan.sun@boonray.com>
2021-12-23 14:37:48 +0800

    二叉树广度优先遍历

--- algorithm/tree/binTreeTraverse.go
+++ algorithm/tree/binTreeTraverse.go
@@ -1,6 +1,9 @@
 package tree
 
-import "strconv"
+import (
+	"container/list"
+	"strconv"
+)
 
 func (node *BinaryTreeNode) Preorder() string {
 	// 先序遍历
@@ -34,6 +37,7 @@ func (node *BinaryTreeNode) Inorder() string {
 }
 
 func (node *BinaryTreeNode) Postorder() string {
+	// 后序遍历
 	var str string = ""
 
 	if node == nil {
@@ -45,4 +49,41 @@ func (node *BinaryTreeNode) Postorder() string {
 	str += strconv.Itoa(node.data)
 
 	return str
+}
+
+func (node *BinaryTreeNode) BreadthFirst() string {
+	// 广度优先遍历
+
+	queue := list.New()
+
+	str := ""
+
+	if node == nil {
+		return str
+	}
+
+	queue.PushBack(node)
+
+	for queue.Len() != 0 {
+		temp := queue.Front()
+		queue.Remove(temp)
+
+		// go 断言
+		if tempNode, ok := temp.Value.(*BinaryTreeNode); ok {
+
+			str += strconv.Itoa(tempNode.data)
+
+			if tempNode.left != nil {
+				queue.PushBack(tempNode.left)
+			}
+
+			if tempNode.right != nil {
+				queue.PushBack(tempNode.right)
+			}
+		}
+
+	}
+
+	return str
+
 }
